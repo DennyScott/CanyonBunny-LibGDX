a8908ffcd3f5388bdef5709282601312bb82e66b
Denny Scott <dennyscott301@gmail.com>
2014-11-29 09:27:20 -0600

    Can Make a Blue Screen Game Appear

--- core/src/com/zephy/canyonbunny/CanyonBunnyMain.java
+++ core/src/com/zephy/canyonbunny/CanyonBunnyMain.java
@@ -1,41 +1,65 @@
 package com.zephy.canyonbunny;
 
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.zephy.canyonbunny.game.WorldController;
+import com.zephy.canyonbunny.game.WorldRenderer;
 
 public class CanyonBunnyMain extends ApplicationAdapter {
 	private WorldController worldController;
 	private WorldRenderer worldRenderer;
-	
+	private boolean paused;
+
 	@Override
 	public void create () {
-		
+		//Set Libgdx log level to DEBUG
+
+		Gdx.app.setLogLevel(Application.LOG_DEBUG);
+
+		//Initalize controller and renderer
+		worldController = new WorldController();
+		worldRenderer = new WorldRenderer(worldController);
+
+		//Game world is active on start
+		paused = false;
 	}
 
 	@Override
 	public void render () {
-		
+		//Do not update game world when paused
+		if(!paused){
+			//Update game world by the time that has passed since last rendered frame
+			worldController.update(Gdx.graphics.getDeltaTime());
+			//Sets the clear screencolor to:Cornflower Blue
+			Gdx.gl.glClearColor(0x64/255.0f, 0x95/255.0f, 0xed/255.0f, 0xff/255.0f);
+			//Clears teh Screen
+			Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+			//Render game world to screen
+			worldRenderer.render();
+		}
+
 	}
-	
+
 	@Override
 	public void resize(int width, int height) {
-		
+		worldRenderer.resize(width, height);
 	}
-	
+
 	@Override
 	public void pause() {
-		
+		paused = true;
 	}
-	
+
 	@Override
 	public void resume() {
-		
+		paused = false;
 	}
-	
+
 	@Override public void dispose() {
-		
+		worldRenderer.dispose();
 	}
 }
