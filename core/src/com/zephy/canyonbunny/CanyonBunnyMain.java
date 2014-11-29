60d3160209ae50c83ffc8d4d8c4ce6194443f3f4
Denny Scott <dennyscott301@gmail.com>
2014-11-29 09:21:53 -0600

    Added base structure from book

---
+++ core/src/com/zephy/canyonbunny/CanyonBunnyMain.java
@@ -0,0 +1,41 @@
+package com.zephy.canyonbunny;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class CanyonBunnyMain extends ApplicationAdapter {
+	private WorldController worldController;
+	private WorldRenderer worldRenderer;
+	
+	@Override
+	public void create () {
+		
+	}
+
+	@Override
+	public void render () {
+		
+	}
+	
+	@Override
+	public void resize(int width, int height) {
+		
+	}
+	
+	@Override
+	public void pause() {
+		
+	}
+	
+	@Override
+	public void resume() {
+		
+	}
+	
+	@Override public void dispose() {
+		
+	}
+}
---
+++ desktop/src/com/zephy/canyonbunny/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.zephy.canyonbunny.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.zephy.canyonbunny.CanyonBunnyMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.height = 480;
+		config.width = 800;
+		new LwjglApplication(new CanyonBunnyMain(), config);
+	}
+}
