9e6ac77e4048e8f2cafeefb02fab73124e7db347
Denny Scott <dennyscott301@gmail.com>
2014-11-30 09:50:14 -0600

    Added Test sprites to Game World

--- core/src/com/zephy/canyonbunny/game/WorldRenderer.java
+++ core/src/com/zephy/canyonbunny/game/WorldRenderer.java
@@ -1,8 +1,10 @@
 package com.zephy.canyonbunny.game;
 
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.Disposable;
+import com.zephy.canyonbunny.util.Constants;
 
 public class WorldRenderer implements Disposable{
 	
@@ -11,15 +13,38 @@ public class WorldRenderer implements Disposable{
 	private WorldController worldController;
 	
 	public WorldRenderer (WorldController worldController) {
-		
+		this.worldController = worldController;
+		init();
 	}
 	
-	private void init() { }
+	private void init() {
+		batch = new SpriteBatch();
+		camera = new OrthographicCamera(Constants.VIEWPORT_WIDTH, Constants.VIEWPORT_HEIGHT);
+		camera.position.set(0,0,0);
+		camera.update();
+	}
 	
-	public void render () { }
-	public void resize (int width, int height){ }
+	public void render () { 
+		renderTestObjects();
+	}
+	
+	private void renderTestObjects() {
+		batch.setProjectionMatrix(camera.combined);
+		batch.begin();
+		for(Sprite sprite: worldController.testSprites){
+			sprite.draw(batch);;
+		}
+		batch.end();
+	}
+	
+	public void resize (int width, int height){
+		camera.viewportWidth = (Constants.VIEWPORT_HEIGHT / height) * width;
+		camera.update();
+	}
 	
 	@Override
-	public void dispose() { }
+	public void dispose() { 
+		batch.dispose();
+	}
 
 }
