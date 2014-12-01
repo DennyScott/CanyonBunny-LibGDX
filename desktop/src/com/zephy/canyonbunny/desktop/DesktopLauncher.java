e0f6b7df239df0e4b243aa4645e5fc06336e3f60
Denny Scott <dennyscott301@gmail.com>
2014-11-30 23:22:36 -0600

    Added Texture Packing

--- desktop/src/com/zephy/canyonbunny/desktop/DesktopLauncher.java
+++ desktop/src/com/zephy/canyonbunny/desktop/DesktopLauncher.java
@@ -2,10 +2,24 @@ package com.zephy.canyonbunny.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker.Settings;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
 import com.zephy.canyonbunny.CanyonBunnyMain;
 
 public class DesktopLauncher {
+	
+	private static boolean rebuildAtlas = true;
+	private static boolean drawDebugOutline = true;
+	
 	public static void main (String[] arg) {
+		if(rebuildAtlas){
+			Settings settings = new Settings();
+			settings.maxWidth = 1024;
+			settings.maxHeight = 1024;
+			settings.debug = drawDebugOutline;
+			TexturePacker.process(settings, "assets-rw/images", "../CanyonBunny-android/assets/images", "canyonbunny.pack");
+		}
+		
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.height = 480;
 		config.width = 800;
